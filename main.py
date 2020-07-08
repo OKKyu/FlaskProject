5df2b121081c19f91af01328f52b834ea56e4ac1
Kiwamu Nakasone <kyukyuinskt@gmail.com>
2020-07-08 15:43:06 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+from pathlib import Path
+from flask import Flask, render_template, Markup, request, jsonify
+import logging
+app = Flask(__name__)
+mylogger = logging.getLogger("FlaskProject")
+
+@app.route("/")
+def hello():
+    name ="ME!"
+    items = ["aa", "bb", "ccc"]
+    common = {"siteTitle":"sample flask website"}
+    return render_template("views/index.html", name=name, items=items, common=common)
+
+@app.route('/user/<username>')
+def show_user_name(username):
+    name ="inputed user name is...." + username
+    return render_template("views/index.html", name=name)
+
+@app.route('/plus/<int:numb>')
+def calcPlus(numb):
+    numb = numb + 4
+    name = "plus result:" + str(numb)
+    return render_template("views/index.html", name=name)
+
+@app.route('/markupSample')
+def markupSample():
+    #Markup return webelement object. but replaced Parameter String has markup, then escape simply str.
+    return Markup('<strong>Hello {}!</strong>').format('<blink>hacker</blink>')
+
+if __name__ == '__main__':
+    #app.run()
+    app.run(host='0.0.0.0', debug=True, port=5002)
